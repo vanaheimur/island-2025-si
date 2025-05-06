deb7ba6df679ac85e3938e18cc7021748806aeba
Stefan Harald <stefan@vanir.is>
2025-05-06 19:34:12 +0000

    Made auth use national registry

--- apps/backend/src/auth/auth.resolver.ts
+++ apps/backend/src/auth/auth.resolver.ts
@@ -4,29 +4,43 @@ import { GraphqlResponse } from './decorators/GraphqlResponse.decorator'
 import { IsPublic } from './decorators/isPublic.decorator'
 import { AuthInput } from './dto/auth.input'
 
+import { Inject, UnauthorizedException } from '@nestjs/common'
 import { Args, Mutation, Resolver } from '@nestjs/graphql'
+import { type Logger, LOGGER_PROVIDER } from '@repo/logger'
 import type { Response } from 'express'
 
 @Resolver()
 export class AuthResolver {
-  constructor(private readonly authService: AuthService) {}
+  constructor(
+    private readonly authService: AuthService,
+    @Inject(LOGGER_PROVIDER) private readonly logger: Logger,
+  ) {}
 
   @IsPublic()
   @Mutation(() => Boolean)
-  login(
+  async login(
     @Args('input') input: AuthInput,
     @GraphqlResponse() response: Response,
-  ): boolean {
-    const accessToken = this.authService.createAuthToken(input)
+  ): Promise<boolean> {
+    try {
+      const accessToken = await this.authService.createAuthToken(
+        input.nationalId,
+      )
 
-    // We set a secure cookie that the frontend js can't access
-    response.cookie(ACCESS_TOKEN, accessToken, {
-      httpOnly: true,
-      secure: true,
-      sameSite: 'strict',
-    })
+      // We set a secure cookie that the frontend js can't access
+      response.cookie(ACCESS_TOKEN, accessToken, {
+        httpOnly: true,
+        secure: true,
+        sameSite: 'strict',
+      })
 
-    return true
+      return true
+    } catch (error) {
+      this.logger.error('Error while logging in', error)
+      throw new UnauthorizedException(
+        'We could not find you, please question your existence to the appropriate authorities',
+      )
+    }
   }
 
   @Mutation(() => Boolean)
--- apps/backend/src/auth/auth.service.ts
+++ apps/backend/src/auth/auth.service.ts
@@ -1,16 +1,28 @@
-import { User } from './types'
+import { NationalRegistryService } from '../national-registry/national-registry.service'
 
-import { Injectable } from '@nestjs/common'
+import { Inject, Injectable } from '@nestjs/common'
 import { JwtService } from '@nestjs/jwt'
+import { type Logger, LOGGER_PROVIDER } from '@repo/logger'
 
 @Injectable()
 export class AuthService {
-  constructor(private readonly jwtService: JwtService) {}
+  constructor(
+    @Inject(LOGGER_PROVIDER) private readonly logger: Logger,
+    private readonly jwtService: JwtService,
+    private readonly nationalRegistryService: NationalRegistryService,
+  ) {}
 
-  public createAuthToken(payload: User) {
+  public async createAuthToken(nationalId: string) {
+    const person = await this.nationalRegistryService.getPersonById(nationalId)
+
+    // In a real application you would separate logs into audit and system logs
+    this.logger.info('Authenticating user', {
+      nationalId,
+    })
+
     return this.jwtService.sign({
-      sub: payload.nationalId,
-      name: payload.name,
+      sub: nationalId,
+      name: person.name,
     })
   }
 }
