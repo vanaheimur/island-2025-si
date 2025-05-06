900b502a8c743ffbae45c85cf373a80f65e7f893
Stefán Harald <stefanhar@users.noreply.github.com>
2025-05-06 19:34:47 +0000

    feat: use national registry in auth (#6)

--- apps/backend/src/auth/dto/auth.input.ts
+++ apps/backend/src/auth/dto/auth.input.ts
@@ -1,13 +1,9 @@
 import { IsNationalId } from '../validators/isNationalId.validator'
 
 import { InputType } from '@nestjs/graphql'
-import { IsString } from 'class-validator'
 
 @InputType()
 export class AuthInput {
-  @IsString()
-  name!: string
-
   @IsNationalId()
   nationalId!: string
 }
